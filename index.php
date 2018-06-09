b9dd3d9fdd735e338f8d543c6dc32a7f64f285e6
Jeanne <jluningp@andrew.cmu.edu>
2018-06-09 19:19:12 -0400

    removing website stuff

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-      <h1>SML Style Checker</h1>
-    Select SML file to upload:
-      <input type="file" name="fileToUpload" id="fileToUpload">
-      <input type="submit" value="Upload SML File" name="submit">
-</form>
-</html>
--- mkrunner.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-print("Script running...")
-print(sys.argv[1])
-f = open("tmp/runner.sml", "w")
-f.write("val _ = markAppends \"" + sys.argv[1] + "\"")
-f.close()
